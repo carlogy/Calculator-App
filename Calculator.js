d10c24d52d46047896c40b58af40bffc8c6064e3
Carlo Yannuzzi <carlogyannuzzi@gmail.com>
2023-12-17 07:55:15 -0500

    cleaned up calculator module.

--- Calculator.js
+++ Calculator.js
@@ -1,10 +1,4 @@
 
-// const buttons = window.parent.document.querySelector(".action-buttons-grid").children;
-
-// Calculator Operations
-
-
-
 function add(num1, num2){
     return num1 + num2;
 }
@@ -26,7 +20,6 @@ function tipFormCalc(total, splitByNumber, tipPercetage) {
 
 }
 
-
 function discountFormCalc(itemPrice, discountPercentage, taxPercentage) {
 
 
@@ -36,24 +29,16 @@ function discountFormCalc(itemPrice, discountPercentage, taxPercentage) {
 
 }
 
-// function  Calculate(num1, operator, num2) {
-
-
-//     try {
-
-//         return operator(num1, num2)
-
-//     } catch (error) {
-
-//        console.log(error);
-//     }
-
-// }
-
-
 function Calculate(num1, operation, num2) {
 
+    try{
+
     return operation(num1, num2);
+
+    } catch (error) {
+
+        console.log(error)
+    }
 }
 
 export {
